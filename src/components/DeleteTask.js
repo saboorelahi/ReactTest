d6f79e7c41a3c245646623d283b3bad73acfef6a
Saboor Elahi <selahi@ssilhr.com.pk>
2021-06-30 03:37:03 +0500

    2:30 hrs - Delete functionality added

--- src/components/DeleteTask.js
+++ src/components/DeleteTask.js
@@ -1,18 +1,44 @@
 import { useState, useEffect } from "react";
 
 const DeleteTask = () => {
+  const [uncheckedTasks, setUnCheckedTasks] = useState([]);
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
     const tasks = JSON.parse(window.localStorage.getItem("tasks"));
     setTasks(tasks);
+		setUnCheckedTasks(tasks);
   }, []);
+	
+  const updateCheckedTasks = (e) => {
+    const { value: index, checked } = e.target;
+
+    if (!checked) {
+      setUnCheckedTasks([...uncheckedTasks, tasks[index]]);
+      return;
+    }
+
+    const updatedCheckedList = [...uncheckedTasks];
+    updatedCheckedList.splice(index, 1);
+		setUnCheckedTasks(updatedCheckedList);
+  };
+
+	const deleteTasks = () => {
+		const updatedTasks = [ ...uncheckedTasks ];
+		setTasks(updatedTasks);
+    window.localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+	};
 
   return (
     <div>
-      <div>
-				{/* In progress delete */}
-      </div>
+      {tasks.map(({ name, id }, index) => (
+        <div key={id}>
+          <input type="checkbox" value={index} onClick={updateCheckedTasks} />
+          <span>{name}</span>
+        </div>
+      ))}
+			<br />
+			<button onClick={deleteTasks}>Delete</button>
     </div>
   );
 };
